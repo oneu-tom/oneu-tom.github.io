b12fac59171802ff93a122f4bf3c53506f7ef1e9
michael <michael@aaa.com>
2024-12-01 18:25:23 +0800

    -

--- env/env.js
+++ env/env.js
@@ -1,5 +1,5 @@
 window.$ou = {
     termsOfService: 'https://oneu.com/terms-of-services',
-    apiBaseUrl: 'http://localhost:5173',
+    apiBaseUrl: 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
     secondOffset: 0
 };
