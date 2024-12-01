21c4487c37059e2b07d35e359338a736323aedf5
michael <michael@aaa.com>
2024-12-01 18:30:26 +0800

    -

--- env/env.js
+++ env/env.js
@@ -1,5 +1,6 @@
+var isDev = window.location.hostname.match(/localhost|192.168/)
 window.$ou = {
     termsOfService: 'https://oneu.com/terms-of-services',
-    apiBaseUrl: 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
+    apiBaseUrl: isDev ? "http://localhost:5173" : 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
     secondOffset: 0
 };
