6034275c3ed3b9abb00c0a7fab8a2826fe14e80f
michael <michael@aaa.com>
2024-12-04 11:51:26 +0800

    -

--- env/env.js
+++ env/env.js
@@ -1,6 +1,6 @@
 var isDev = window.location.hostname.match(/localhost|192.168/)
 window.$ou = {
     termsOfService: 'https://oneu.com/terms-of-services',
-    apiBaseUrl: isDev ? "http://localhost:5173" : 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
+    apiBaseUrl: 'https://oneu.tv',
     secondOffset: 0
 };
