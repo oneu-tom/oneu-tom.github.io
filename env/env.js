1fba1f5644807ca6e6bfd20415ad03304a6bb159
michael <michael@aaa.com>
2024-12-10 12:17:29 +0800

    -

--- env/env.js
+++ env/env.js
@@ -3,5 +3,6 @@ window.$ou = {
     termsOfService: 'https://oneu.com/terms-of-services',
     // apiBaseUrl: isDev ? "http://localhost:5173" : 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
     apiBaseUrl: 'https://oneu.tv',
+    wsBaseUrl: 'ws://ec2-13-54-6-236.ap-southeast-2.compute.amazonaws.com:8080',
     secondOffset: 0
 };
