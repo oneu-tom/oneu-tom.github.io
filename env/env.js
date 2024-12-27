164a3073dfecb531c61ad7dadb38dccf127bf0c4
michael <michael@aaa.com>
2024-12-28 00:06:53 +0800

    -

--- env/env.js
+++ env/env.js
@@ -8,7 +8,7 @@ window.$ou = {
     TMAUrl: 'https://t.me/tmxiongBot/oneU',
     secondOffset: 0,
     manifestUrl: 'https://oneu-tom.github.io/tonconnect-manifest.json',
-    canSkipLogin: true,
+    canSkipLogin: false,
     // 商户id
     tenantId: '108432298226089985'
 };
