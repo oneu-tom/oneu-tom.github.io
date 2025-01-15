27c4212bb876a9311d264d188750176cb5faeb45
michael <michael@aaa.com>
2025-01-16 00:23:27 +0800

    -

--- env/env.js
+++ env/env.js
@@ -4,6 +4,7 @@ window.$ou = {
     // apiBaseUrl: isDev ? "http://localhost:5173" : 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
     // apiBaseUrl: 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
     apiBaseUrl: 'https://oneu.tv',
+    // apiBaseUrl: 'https://api.oneu66.com',
     wsBaseUrl: 'wss://oneu.tv/ws',
     TMAUrl: 'https://t.me/tmxiongBot/oneU',
     secondOffset: 0,
@@ -13,5 +14,5 @@ window.$ou = {
     tenantId: '111340666742689792',
     // 最低提现佣金
     minCommission: 10,
-    version: '1.1.0'
+    version: '1.0.0'
 };
