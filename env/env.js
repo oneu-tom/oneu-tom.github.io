cc85a28fff15a8dfacdc24a59a53b003324e5ad3
michael <michael@aaa.com>
2025-01-16 16:39:01 +0800

    -

--- env/env.js
+++ env/env.js
@@ -1,17 +1,16 @@
 var isDev = window.location.hostname.match(/localhost|192.168/)
 window.$ou = {
     termsOfService: 'https://oneu.com/terms-of-services',
-    // apiBaseUrl: isDev ? "http://localhost:5173" : 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
-    // apiBaseUrl: 'http://ec2-13-55-71-77.ap-southeast-2.compute.amazonaws.com',
-    apiBaseUrl: 'https://oneu.tv',
-    // apiBaseUrl: 'https://api.oneu66.com',
+    // apiBaseUrl: 'https://oneu.tv',
     wsBaseUrl: 'wss://oneu.tv/ws',
+    apiBaseUrl: 'https://api.oneu66.com',
+    // wsBaseUrl: 'wss://api.oneu66.com/game',
     TMAUrl: 'https://t.me/tmxiongBot/oneU',
     secondOffset: 0,
     manifestUrl: 'https://oneu-tom.github.io/tonconnect-manifest.json',
     canSkipLogin: false,
     // 商户id
-    tenantId: '111340666742689792',
+    tenantId: '115581230891663360',
     // 最低提现佣金
     minCommission: 10,
     version: '1.0.0'
