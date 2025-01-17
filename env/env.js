ae288d03876d29b28408f731be064543c26689e4
michael <michael@aaa.com>
2025-01-17 19:04:47 +0800

    -

--- env/env.js
+++ env/env.js
@@ -2,9 +2,9 @@ var isDev = window.location.hostname.match(/localhost|192.168/)
 window.$ou = {
     termsOfService: 'https://oneu.com/terms-of-services',
     // apiBaseUrl: 'https://oneu.tv',
-    wsBaseUrl: 'wss://oneu.tv/ws',
+    // wsBaseUrl: 'wss://oneu.tv/ws',
     apiBaseUrl: 'https://api.oneu66.com',
-    // wsBaseUrl: 'wss://api.oneu66.com/game',
+    wsBaseUrl: 'wss://api.oneu66.com/game/ws',
     TMAUrl: 'https://t.me/tmxiongBot/oneU',
     secondOffset: 0,
     manifestUrl: 'https://oneu-tom.github.io/tonconnect-manifest.json',
