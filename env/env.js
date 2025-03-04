8845cc5711d05f23b918bdec2da932f014380b7a
michael <michael@aaa.com>
2025-03-04 23:33:04 +0800

    -

--- env/env.js
+++ env/env.js
@@ -1,19 +1,20 @@
 var isDev = window.location.hostname.match(/localhost|192.168/)
 window.$ou = {
-
+    // api接口地址
     apiBaseUrl: 'https://api.oneu66.com',
+    // ws接口地址
     wsBaseUrl: 'wss://api.oneu66.com/game/ws',
+    // TG邀请链接
     TMAUrl: 'https://t.me/tmxiongBot/oneU',
+    // ton钱包连接时显示图标与名称
     manifestUrl: 'https://oneu-tom.github.io/tonconnect-manifest.json',
-
+    // 商户id
+    tenantId: '131847238522515456',
 
 
     termsOfService: 'https://oneu.com/terms-of-services',
     // 时间差
     secondOffset: 0,
-    canSkipLogin: false,
-    // 商户id
-    tenantId: '131847238522515456',
     // 最低提现佣金
     minCommission: 10,
     // 最大购买数量
